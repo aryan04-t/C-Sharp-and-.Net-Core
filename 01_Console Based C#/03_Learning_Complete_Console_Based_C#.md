feef9bcd2554fca35af1bf4ef18f439dd9f1ef51
Aryan Tomar <aryantomar5000@gmail.com>
2024-05-01 15:23:14 +0530

    Practice code of Console Based C#

--- 01_Console Based C#/03_Learning_Complete_Console_Based_C#.md
+++ 01_Console Based C#/03_Learning_Complete_Console_Based_C#.md
@@ -4,6 +4,8 @@
 
 - If you know C++, Java, and JavaScript already then it is just 1 day task to learn complete console based C#, there is barely anything new in C# other then these 3 languages, it is just that C# feels like a combination of all the above mentioned 3 Programming Languages. 
 
+- C# (C Sharp) programs are saved with .cs file extension 
+
 
 
 # References: 
---
+++ 01_Console Based C#/practiceCode/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int age = 20; 
+float randomNum = 100.234F; 
+string name = "Aryan Tomar"; 
+char gender = 'M'; 
+
+Console.WriteLine(); 
+Console.WriteLine("Name: " + name + ", Age: " + age);
+
+if(gender == 'M'){
+    Console.WriteLine(name + " is a Male");
+}
+else if(gender == 'F'){
+    Console.WriteLine(name + "is a Female");
+} 
+
+Console.WriteLine("This is a random number: " + randomNum);
+Console.WriteLine(); 
